346c8d474b5978fc8b8425f90f8aa052ccee01c7
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-06-02 03:22:45 +0000

    Add user profile image attachment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  has_one_attached :image
   has_secure_password
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
-  
+
   validates :name, presence: true
   validates :email_address, presence: true, uniqueness: true
 
