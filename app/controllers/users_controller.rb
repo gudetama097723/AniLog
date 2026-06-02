3fa8cd8f2c5272a9aa6e7d4263b528817f531d55
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-06-02 06:35:24 +0000

    Add user account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   allow_unauthenticated_access only: [:new, :create]
 
+  before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
+
+  def mypage
+    @user = Current.user
+    @anime_reviews = @user.anime_reviews.order(created_at: :desc)
+  end
+
   def new
     @user = User.new
   end
@@ -15,10 +23,42 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_update_params)
+      redirect_to mypage_path, notice: "ユーザー情報を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    cookies.delete(:session_id)
+    redirect_to new_user_path
+  end
+
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def authorize_user
+    unless @user == Current.user
+      redirect_to mypage_path, alert: "権限がありません。"
+    end
+  end
 
   def user_params
     params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
+  end
+
+  def user_update_params
+    params.require(:user).permit(:name, :email_address)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-
-  get "users/new"
   resource :session
   resources :passwords, param: :token
-  resources :users, only: [:new, :create]
-  resources :anime_reviews
 
   root "homes#top"
   get "about" => "homes#about"
+  get "mypage" => "users#mypage"
+
+  resources :users, only: [:new, :create, :edit, :update, :destroy]
+  resources :anime_reviews
 
   get "up" => "rails/health#show", as: :rails_health_check
 
